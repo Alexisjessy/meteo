49437340608e6c7f908637ce33e2a21188f3199e
Alexisjessy <152199435+Alexisjessy@users.noreply.github.com>
2024-03-18 13:00:22 +0100

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -4,7 +4,7 @@
 
 function getWeather(city) {
    
-    const apiKey = API_KEY;
+    const apiKey = '7e357635340d302c820ee5beca60af26';
 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=fr&units=metric`;
 
